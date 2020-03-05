cb62faf300e48b92b770e960af24af0d7034a126
HONG LIN <lhfoothill@gmail.com>
2020-03-05 15:57:13 -0600

    some changes

---
+++ react-lazy-load/src/index.js
@@ -0,0 +1,18 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Artists = lazy(() => import('./Artists'))
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Suspense fallback={<h1>LOADING...</h1>}>
+            <Artists/>
+        </Suspense>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- shell/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import reducer from "../reducers/reducer";
-
-const middleware = applyMiddleware;
-const store = createStore(reducer, middleware(thunk));
-export default store;
-
